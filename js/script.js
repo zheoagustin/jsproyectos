386f142d362c08a99d791f14e69fbe613808fbbb
Javier Primo <japsgy@gmail.com>
2022-05-02 03:03:05 -0300

    Primer desafio complementario corregido

--- js/script.js
+++ js/script.js
@@ -1,37 +1,25 @@
-let opcion = parseFloat (prompt ("Ingrese numero del 1 al 4") )
-
-switch (opcion) {
-    case 1: alert ("Ha ingresado la opcion de seleccion de turnos")
-        
-        break;
-    case 2: alert ("Ha ingresado la opcion de seleccion de turnos")
-        
-        break;
-    case 3: alert ("Ha ingresado la opcion de seleccion de turnos")
-        
-     break;
-    case 4: alert ("Ha ingresado la opcion de seleccion de turnos")
-        
-        break;
-
-    default: alert ("Su opcion es incorrecta")
-        break;
-}
-
-alert ("Ingrese Mes del turno, dia y Luego SALIR para su turno")
 let turno = prompt ("Ingrese fecha y mes del turno")
 
 while (turno != "SALIR") {    
     console.log("Se ah agendado un turno para el " + turno );
-   turno = prompt ("Si ingreso correctamente Escriba SALIR, sino repita sus datos")
+    turno = prompt ("Si ingreso correctamente Escriba Salir, sino repita sus datos")
    
 }
-let cuota = 6;
-let tabla = parseInt (prompt ("En cuantas cuotas desea pagar su Asesoramiento"))
-for (let i=1; i<=tabla; i++) {
-    console.log("En " + i + " cuotas, el interes es del  " + cuota * i + " %"
-    )
 
+let precioFinal = 3000
+let interes = 5
+let pregunta = prompt ("Desea pagar su consulta en cuotas? (Si o No)")
+
+if (pregunta === "SI") {
+    
+    let cuotas = parseInt (prompt ("En cuantas cuotas desea pagar su Asesoramiento"))
+
+    console.log("En " + cuotas + " cuotas, el interes es del  " + interes * cuotas + " %")
+
+    console.log("su consulta tiene un precio final de " + precioFinal +"$");
+    
+    alert ("Su consulta tiene un precio final de: " + precioFinal  + "$, con un interes del " + interes * cuotas + " % en " + cuotas + " cuotas")
 }
-console.log("su consulta tiene un precio final de " + 300 * tabla +"$");
-alert ("Su consulta tiene un precio final de: " + 300 * tabla + "$, consulte el interes en la consola de su navegador")
+else {
+    alert ("Su precio final es de " + precioFinal)
+}
